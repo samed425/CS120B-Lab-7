9025a2b26a5862cc344255aeb9ed0ed4b6efbd7d
Bahman Baidar <bbaid001@wch136-22.cs.ucr.edu>
2021-02-06 19:44:06 -0800

    Part 2 first try

---
+++ turnin/bbaid001_lab7_part2.c
@@ -0,0 +1,37 @@
+/*	Author: bbaid001
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ *
+ *	Demo Link: 
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+void ADC_init() {
+    ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+}
+
+int main(void) {
+    DDRA = 0x00; PINA = 0xFF;
+    DDRB = 0xFF; PINB = 0x00;
+    DDRD = 0xFF; PIND = 0x00;
+    unsigned char led_b;
+    unsigned char led_d;
+    unsigned short temp;
+    ADC_init();
+    while (1) {
+	temp = ADC;
+	led_b = (char) temp;
+	led_d = (char)(temp >> 8);
+	PORTB = led_b;
+	PORTD = led_d;
+    }
+    return 1;
+}
