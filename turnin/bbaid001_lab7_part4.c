dff98b325e3c047e9b929ad27e6819a1c75a2b2e
samed425 <77028650+samed425@users.noreply.github.com>
2021-02-07 17:57:59 -0800

    Update bbaid001_lab7_part4.c

--- turnin/bbaid001_lab7_part4.c
+++ turnin/bbaid001_lab7_part4.c
@@ -7,7 +7,7 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  *
- *	Demo Link: 
+ *	Demo Link: https://www.youtube.com/watch?v=40k6oHQHOdw
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
